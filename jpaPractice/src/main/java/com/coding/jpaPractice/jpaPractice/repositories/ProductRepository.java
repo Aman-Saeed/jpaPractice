221b908b30c613854c92a3dce5b58ea08cae0dcb
Amanullah Saeed <55559732+Aman-Saeed@users.noreply.github.com>
2026-07-06 22:53:26 +0430

    Spring Data JPA Interfaces, Dynamic Query method and custom query

--- jpaPractice/src/main/java/com/coding/jpaPractice/jpaPractice/repositories/ProductRepository.java
+++ jpaPractice/src/main/java/com/coding/jpaPractice/jpaPractice/repositories/ProductRepository.java
@@ -1,4 +1,28 @@
 package com.coding.jpaPractice.jpaPractice.repositories;
 
-public interface ProductRepository {
+import com.coding.jpaPractice.jpaPractice.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    ProductEntity findByTitle(String title);
+
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime createdAt);
+
+
+    //List<ProductEntity> findByQuantityAndPrice(int quantity, BigDecimal price);
+
+    @Query("SELECT p FROM ProductEntity p WHERE p.quantity = ?1 AND p.price = ?2")
+    List<ProductEntity> findByQuantityAndPrice(int quantity, BigDecimal price);
+
+    List<ProductEntity> findByTitleContaining(String title);
+
+    List<ProductEntity> findByTitleContainingIgnoreCase(String title);
 }
--- jpaPractice/src/test/java/com/coding/jpaPractice/jpaPractice/JpaPracticeApplicationTests.java
+++ jpaPractice/src/test/java/com/coding/jpaPractice/jpaPractice/JpaPracticeApplicationTests.java
@@ -1,13 +1,68 @@
 package com.coding.jpaPractice.jpaPractice;
 
+import com.coding.jpaPractice.jpaPractice.entities.ProductEntity;
+import com.coding.jpaPractice.jpaPractice.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 class JpaPracticeApplicationTests {
 
+    @Autowired
+    ProductRepository productRepository;
 	@Test
 	void contextLoads() {
 	}
 
+    @Test
+    void testProductRepository() {
+
+        ProductEntity productEntity = ProductEntity.builder()
+                .sku("SKU123")
+                .title("Sample Product")
+                .price(BigDecimal.valueOf(19.99))
+                .quantity(100)
+                .build();
+
+        ProductEntity savedProduct = productRepository.save(productEntity);
+        System.out.println("Saved Product: " + savedProduct);
+    }
+
+    @Test
+    void testFindAllProducts() {
+        System.out.println("All Products: " + productRepository.findAll());
+    }
+
+    @Test
+    void testFindProductByTitle(){
+       ProductEntity product = productRepository.findByTitle("Pepsi");
+        System.out.println("Found Product by Title: " + product);
+    }
+
+    @Test
+    void testFindByCreatedAtAfter(){
+        List<ProductEntity> products = productRepository.findByCreatedAtAfter(
+                LocalDateTime.of(2027, 1, 1, 0, 0, 0));
+            System.out.println("Found Products by Created At After: " + products);
+
+    }
+
+    @Test
+    void testFindByQuantityAndPrice(){
+        List<ProductEntity> products = productRepository.findByQuantityAndPrice(10, BigDecimal.valueOf(15.6));
+        System.out.println("Found Products by Quantity and Price: " + products);
+    }
+
+    @Test
+    void testTitleContaining(){
+        //List<ProductEntity> products = productRepository.findByTitleContaining("psi");
+        List<ProductEntity> products = productRepository.findByTitleContainingIgnoreCase("psi");
+
+        System.out.println("Found Products by Title Containing: " + products);
+    }
 }
