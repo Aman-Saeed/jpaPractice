595fc4590da415d5bf3d518103b58008f987f56c
Amanullah Saeed <55559732+Aman-Saeed@users.noreply.github.com>
2026-07-07 12:30:14 +0430

    created new jpaPractice, added JPA custom and dynamic methods

--- jpaPractice/src/main/java/com/coding/jpaPractice/JpaPracticeApplication.java
+++ jpaPractice/src/main/java/com/coding/jpaPractice/JpaPracticeApplication.java
@@ -1,4 +1,4 @@
-package com.coding.jpaPractice.jpaPractice;
+package com.coding.jpaPractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
